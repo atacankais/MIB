f2cc986d6c7476f391cd8046a3e691dca9c2de09
Sayed <jafarosmani10@gmail.com>
2022-12-18 13:07:41 +0100

    h

--- src/MIB/HuvudMenyAgent.java
+++ src/MIB/HuvudMenyAgent.java
@@ -42,6 +42,11 @@ public class HuvudMenyAgent extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("Registrera ny alien");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Ändra alieninformation");
 
@@ -114,6 +119,10 @@ public class HuvudMenyAgent extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
